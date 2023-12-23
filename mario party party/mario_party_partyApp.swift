12f79d486f0c5605e61997549114ae74389ed260
Roman Bucher <rbucher@deloitte.dk>
2023-12-23 14:08:07 +0100

    Initial Commit

---
+++ mario party party/mario_party_partyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mario_party_partyApp.swift
+//  mario party party
+//
+//  Created by Roman Bucher on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct mario_party_partyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
