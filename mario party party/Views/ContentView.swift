f23457afcbe0fe36eb0b241eee710ded2fda7194
Roman Bucher <rbucher@deloitte.dk>
2024-01-19 13:24:51 +0100

    refactor

--- mario party party/Views/ContentView.swift
+++ mario party party/Views/ContentView.swift
@@ -14,33 +14,32 @@ struct ContentView: View {
     @ObservedObject var userModel = UserModel()
     @ObservedObject var scoreModel = ScoreModel()
     @State var addingNewScores = false
-        
+    
     var body: some View {
         VStack {
             Text("MARIO PARTY").font(Font.custom("Lemon-Regular", size: 32)).padding(.bottom)
+            VStack {
                 VStack {
-                        VStack {
-                            ForEach(userModel.usersSortedByScore()) { user in
-                                HStack {
-                                    Text(user.name).font(Font.custom("Lemon-Regular", size: 18)).padding(.leading)
-                                Spacer()
-                                    Text(String(user.totalScore)).font(Font.custom("Lemon-Regular", size: 18)).padding(.trailing)
-                                }
-                            }.padding(.bottom)
-                    }
+                    ForEach(userModel.usersSortedByScore()) { user in
+                        HStack {
+                            Text(user.name).font(Font.custom("Lemon-Regular", size: 18)).padding(.leading)
+                            Spacer()
+                            Text(String(user.totalScore)).font(Font.custom("Lemon-Regular", size: 18)).padding(.trailing)
+                        }
+                    }.padding(.bottom)
                 }
-                Spacer()
-                Button(action: {
-                    self.addingNewScores.toggle()
-                }) {
-                    Text("Neue Resultate").font(.custom("Lemon-Regular", size: 20))
-                
             }
+            Spacer()
+            Button(action: {
+                self.addingNewScores.toggle()
+            }) {
+                Text("Neue Resultate").font(.custom("Lemon-Regular", size: 24))
+            }.padding().buttonStyle(.bordered).tint(.orange)
         }.sheet(isPresented: $addingNewScores,
                 content: {
             NewResultsSheetView(users: userModel.users).environmentObject(scoreModel).environmentObject(userModel)
-                }
-        ).font(.custom("Lemon-Regular", size: 18)).padding()
+        }
+        ).font(.custom("Lemon-Regular", size: 16)).padding()
     }
     
     init() {
@@ -56,24 +55,31 @@ struct NewResultsSheetView: View {
     @EnvironmentObject var scoreModel: ScoreModel
     
     @State private var date = Date()
-    @State private var validScore = true
-
+    @State private var invalidScore = false
+    
     @Environment(\.dismiss) var dismiss
     
-    func validateScores() {
+    func validateScores() -> Bool {
         var maxScoreCount = 0
         self.users.forEach { user in
             if (user.score == 7) {
                 maxScoreCount += 1
             }
         }
+        print("maxScoreCount")
+        print(maxScoreCount)
         if (maxScoreCount != 1) {
-            validScore = false
+            print("hello")
+            invalidScore = true
+            return false
         }
+        return true
     }
     
     func storeScore() {
-        
+        if (!validateScores()) {
+            return
+        }
         self.users.forEach { user in
             scoreModel.addScores(
                 score: Score(
@@ -83,26 +89,34 @@ struct NewResultsSheetView: View {
                 ), userModel: userModel)
         }
         dismiss()
-        
     }
     
     var body: some View {
         VStack {
-            DatePicker(
-                "Datum",
-                selection: $date,
-                displayedComponents: [.date, .hourAndMinute]
-            )
+            HStack {
+                Spacer()
+                DatePicker(
+                    selection: $date,
+                    displayedComponents: [.date, .hourAndMinute],
+                    label: { Text("Datum").foregroundStyle(.gray) }
+                )
+            }.padding(.leading).padding(.trailing)
+            Spacer()
+            Divider()
+            Spacer()
             ForEach(users) { user in
                 PlayerSlider2(user: user).environmentObject(userModel)
             }
-            Button(action: storeScore) {
-                Text("Speichern")
+            Spacer()
+            Divider()
+            Spacer()
+            Button("Speichern") {
+                storeScore()
+            }.buttonStyle(.bordered).tint(.green).alert("Ungültiges Resultat", isPresented: $invalidScore) {
+                Button("Ok", role: .cancel) { }
             }
-        }.padding()
-            //.alert("Noooooo", isPresented: $validScore) {
-            //Button("Ok", role: .cancel) { }
-        //}
+        }.padding().presentationDetents([.height(500)])
+        
     }
     
     init(users: [User]) {
@@ -145,8 +159,9 @@ struct PlayerSlider2: View {
     }
     
     var body: some View  {
-        HStack(alignment: .firstTextBaseline) {
+        HStack() {
             Text(user.name)
+            Spacer()
             Slider(
                 value:
                     Binding(
@@ -163,7 +178,7 @@ struct PlayerSlider2: View {
                 onEditingChanged: { editing in
                     isEditing = editing
                 }
-            ).tint(scoreColor())
+            ).tint(scoreColor()).frame(width: 220)
             Text("\(scoreInt)")
                 .foregroundColor(isEditing ? randomColor() : scoreColor())
         }.padding()
